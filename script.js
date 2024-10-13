5e7400f5e8d2abb428fb3aa59a2a8c1019081990
Fernando Rodrigues <117524258+NandaoSpain@users.noreply.github.com>
2024-10-13 11:48:32 -0300

    ferra

---
+++ script.js
@@ -0,0 +1,29 @@
+const form = document.querySelector('form')
+const input = document.querySelector('input')
+const newItem = document.querySelector('ul')
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  
+  let newElementLi = document.createElement('li')
+  let newElementDiv = document.createElement('div')
+  let newElementCheckbox = document.createElement('input')
+  let newElementLabel = document.createElement('label')
+  let newElementButton = document.createElement('button')
+  let newElementIcon = document.createElement('i')
+
+  newElementCheckbox.type = 'checkbox'
+  newElementLabel.textContent = input.value
+  newElementIcon.classList.add('ph', 'ph-trash')
+
+  newElementButton.appendChild(newElementIcon)
+
+  newElementDiv.appendChild(newElementCheckbox)
+  newElementDiv.appendChild(newElementLabel)
+
+  newElementLi.appendChild(newElementDiv)
+  newElementLi.appendChild(newElementButton)
+
+  newItem.appendChild(newElementLi)
+  input.value = ''
+})
